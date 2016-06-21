9025c9499e62edeaf0ca202b89f5bb4f5e14728d
ziahaqi <zia.ulhack@gmail.com>
2016-06-21 13:05:33 +0700

    add test

--- uiflow/src/test/java/id/co/veritrans/sdk/uiflow/ExampleUnitTest.java
+++ uiflow/src/test/java/id/co/veritrans/sdk/uiflow/ExampleUnitTest.java
@@ -1,10 +1,16 @@
 package id.co.veritrans.sdk.uiflow;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+    @Test
     public void addition_isCorrect() throws Exception {
+        junit.framework.Assert.assertEquals(2, 1+1);
+
     }
 }
